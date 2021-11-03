fe76ea44160650c73da9c41059f707751e2a4307
Nils Lutz <nils.lutz@btc-ag.com>
2021-11-03 20:44:48 +0100

    feat(token): updated deployment script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -13,13 +13,27 @@ async function main() {
   // manually to make sure everything is compiled
   // await hre.run('compile');
 
+  const [deployer] = await hre.ethers.getSigners();
+
+  console.log("Deploying contracts with account:", deployer.address);
+
   // We get the contract to deploy
   const Greeter = await hre.ethers.getContractFactory("Greeter");
   const greeter = await Greeter.deploy("Hello, Hardhat!");
 
+  const Token = await hre.ethers.getContractFactory("Token");
+  const token = await Token.deploy();
+
+  const NimbleToken = await hre.ethers.getContractFactory("NimbleToken");
+  const nimbleToken = await NimbleToken.deploy("Nimble Token", "NIT");
+
   await greeter.deployed();
+  await token.deployed();
+  await nimbleToken.deployed();
 
   console.log("Greeter deployed to:", greeter.address);
+  console.log("Token deployed to:", token.address);
+  console.log("Nimble Token deployed to:", nimbleToken.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
